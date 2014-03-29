44e0522ff81aded2721a44dd5d0f648df40f3bcf
Steven Chan <icemanchanx@gmail.com>
2014-03-28 21:38:01 -0400

    Updated Server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hello World: From Node\n');
 }).listen(8888, 'localhost');
 console.log('Server running at http://localhost:8888/');
